46a9a1ec5079ab081ee4b807b0183db19865148a
David Tsuji <david@flodge.com>
2014-06-19 15:00:24 +1000

    merged pierre's work

--- src/index.js
+++ src/index.js
@@ -1,161 +1,33 @@
 var cast = require('sc-cast'),
 	hasKey = require('sc-haskey'),
 	MongoClient = require('mongodb').MongoClient,
+	baseAdapter = require('moldy-base-adapter'),
 	ObjectID = require('mongodb').ObjectID;
 
 var LOGGING_ON = false,
 	DEFAULT_DATABASE = 'MoldyModelsUnnamed';
 
+var config = {
+	connectionString: 'mongodb://127.0.0.1:27017/',
+	databaseName: DEFAULT_DATABASE
+};
 
+var url = config.connectionString + config.databaseName;
 
-module.exports = function (_model, _data, _method, _url, _callback) {
-	var method = _method,
-		model = _model,
-		data = cast(_data, 'object', {});
+var connect = function (callback) {
 
-	log(method, _data);
-	
-	MongoClient.connect('mongodb://127.0.0.1:27017/' + DEFAULT_DATABASE, function(err, db) {
-		if(err) {
-			return _callback(err);
-		}
-		var collectionName = _model.__name;
+	var cs = this.config.connectionString ? this.config.connectionString : config.connectionString,
+		dbName = this.config.databaseName ? this.config.databaseName : config.databaseName;
 
-		var collection = db.collection(collectionName);
-		log('collection', collectionName);
+	MongoClient.connect(cs + dbName, callback);
+};
 
-		switch (true) {
-			case (/get/i.test(method)): // $get or $collection
-
-				var data = moldyToMongo(_data);
-
-				var findOptions = {};
-
-
-				if (data._id) { //Get one item
-
-					collection.findOne({_id: new ObjectID(data._id)}, function(err, dbItem) {
-						if (err) {
-							return _callback(err);
-						}
-						if (!dbItem) {
-							//TODO: handle missing items better.
-							log('Missing item', _data, data, dbItem);
-							return _callback(null, null);
-						}
-						
-						dbItem = mongoToMoldy(dbItem);
-
-						log('$get res', dbItem);
-						
-						db.close();
-
-						_callback(null, dbItem);
-					});
-
-				} else { //Get collection of items
-
-					collection.find(data).toArray(function(err, dbItems) {
-						if (err) {
-							return _callback(err);
-						}
-
-						log('$collection pre-res', dbItems);
-
-						dbItems.forEach(function(dbItem) {
-							dbItem = mongoToMoldy(dbItem);							
-						});
-
-
-						log('$collection res', dbItems);
-						
-						db.close();
-
-						_callback(null, dbItems);
-					});
-
-				}
-
-				break;
-
-			case (/post/i.test(method)): //$create
-
-				var data = moldyToMongo(_data);
-
-				collection.insert(data, function(err, dbItems) {
-					if (err) {
-						return _callback(err);
-					}
-					if (dbItems.length !== 1) {
-						return _callback(new Error('MongoDb returned an unexpected amount of items on insert'));
-					}
-
-					var dbItem = dbItems[0];
-
-					dbItem = mongoToMoldy(dbItem);
-
-					log(method + ' res', dbItem);
-
-					db.close();
-
-					_callback(null, dbItem);
-
-				});
-				
-				break;
-
-			case (/put/i.test(method)): //$update
-				
-				//Copy and remove the id so mongo is happy
-				var data = moldyToMongo(_data);
-
-				collection.save(data, function(err, updateCount) {
-					if (err) {
-						return _callback(err);
-					}
-					if (updateCount != 1) {
-						return _callback(new Error('MongoDb returned an unexpected amount of items on save'));
-					}
-
-					log(method + ' res', updateCount);
-
-					db.close();
-
-					_callback(null, updateCount);
-
-				});
-
-				break;
-
-			case (/delete/i.test(method)): //$destroy
-
-				var data = moldyToMongo(_data);
-
-				collection.remove({_id: new ObjectID(data._id)}, function(err, count) {
-					if (err) {
-						return _callback(err);
-					}
-					
-					log(method + ' res', count);
-					
-					db.close();
-
-					_callback(null, count);
-				});
-				
-				break;
-		}
-
-
-
-		
-	});
-
-}
-
+var getCollection = function (db) {
+	return db.collection(this.__name);
+};
 
 function log() {
-	if(LOGGING_ON){
+	if (LOGGING_ON) {
 		console.log.apply(console, arguments);
 	}
 }
@@ -170,9 +42,168 @@ function mongoToMoldy(item) {
 //Swap IDs to be mongo standard
 function moldyToMongo(item) {
 	var newItem = JSON.parse(JSON.stringify(item));
-	if (newItem.id) {		
-		newItem._id = newItem.id;
+	if (newItem.id) {
+		newItem._id = new ObjectID(newItem.id);
 		delete newItem['id'];
 	}
-	return newItem;	
+	return newItem;
 }
+
+module.exports = baseAdapter.extend({
+	name: "mongodb",
+	create: function (data, done) {
+		var self = this,
+			col;
+
+		connect.call(this.__adapter.mongodb, function (err, db) {
+			if (err) {
+				return done(err);
+			}
+
+			col = getCollection.call(self, db);
+
+			col.insert(moldyToMongo(data), function (err, dbItems) {
+
+
+				if (err) {
+					return done(err);
+				}
+
+				if (dbItems.length !== 1) {
+					return done(new Error('MongoDb returned an unexpected amount of items on insert'));
+				}
+
+				var dbItem = mongoToMoldy(dbItems[0]);
+
+				db.close();
+
+				done(null, dbItem);
+
+			});
+
+		});
+	},
+	findOne: function (query, done) {
+		var self = this,
+			col;
+
+		connect.call(this.__adapter.mongodb, function (err, db) {
+			if (err) {
+				return done(err);
+			}
+			col = getCollection.call(self, db);
+
+			if (query.id) {
+				query = {
+					_id: new ObjectID(query.id)
+				}
+			}
+
+			col.findOne(query, function (err, dbItem) {
+				if (err) {
+					return done(err);
+				}
+				if (!dbItem) {
+					//TODO: handle missing items better.
+					log('Missing item', _data, data, dbItem);
+					return done(null, null);
+				}
+
+				dbItem = mongoToMoldy(dbItem);
+
+				log('$get res', dbItem);
+
+				db.close();
+
+				done(null, dbItem);
+			});
+		});
+	},
+	find: function (query, done) {
+		var self = this,
+			col;
+
+		connect.call(this.__adapter.mongodb, function (err, db) {
+			if (err) {
+				return done(err);
+			}
+
+			col = getCollection.call(self, db);
+
+			col.find(query || {}).toArray(function (err, dbItems) {
+
+				if (err) {
+					return done(err);
+				}
+
+				log('$collection pre-res', dbItems);
+
+				dbItems.forEach(function (dbItem) {
+					dbItem = mongoToMoldy(dbItem);
+				});
+
+
+				log('$collection res', dbItems);
+
+				db.close();
+
+				done(null, dbItems);
+			});
+		});
+	},
+	save: function (data, done) {
+		var self = this,
+			col;
+
+		connect.call(this.__adapter.mongodb, function (err, db) {
+			if (err) {
+				return done(err);
+			}
+
+			col = getCollection.call(self, db);
+
+			//Copy and remove the id so mongo is happy
+			data = moldyToMongo(data);
+
+			col.save(data, function (err, updateCount) {
+				if (err) {
+					return done(err);
+				}
+				if (updateCount != 1) {
+					return done(new Error('MongoDb returned an unexpected amount of items on save'));
+				}
+
+				db.close();
+
+				done(null, updateCount);
+			});
+		});
+	},
+	destroy: function (data, done) {
+		var self = this,
+			col;
+
+		connect.call(this.__adapter.mongodb, function (err, db) {
+			if (err) {
+				return done(err);
+			}
+
+			col = getCollection.call(self, db);
+
+			data = moldyToMongo(data);
+
+			col.remove({
+				_id: new ObjectID(data._id)
+			}, function (err, count) {
+				if (err) {
+					return done(err);
+				}
+
+				db.close();
+
+				done(null, count);
+			});
+
+		});
+	}
+});
