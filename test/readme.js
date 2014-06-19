8bb192160660456ef1d1242b7044522c11ddb0cc
David Tsuji <david@flodge.com>
2014-06-20 09:09:26 +1000

    updated the readme and removed an unused file

--- test/readme.js
+++ test/readme.js
@@ -6,6 +6,8 @@ describe('moldy-mongo-adapter', function () {
 
 	it('Tell `Moldy` to use the `mongo` adapter', function () {
 		// Moldy.use( require('moldy-mongo-adapter') );
+		// Moldy.adapters.mongodb.config.databaseName = 'moldyMongoAdapterTests';
+		// Moldy.adapters.mongodb.config.connectionString = 'mongodb://127.0.0.1:27017/';
 	});
 
 	require('./readme/create');
